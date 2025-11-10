537121d66b30dc87688854212de5ba880781a0d8
Michael <233148174+psyhoopat@users.noreply.github.com>
2025-11-10 20:09:15 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('registration/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/image/', views.load_image, name='load_image'),
+    path('logout/', views.logout_view, name='logout'),
 ]
