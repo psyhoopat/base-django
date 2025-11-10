d7730396c4389807fa7204d1a0a3d85d8a3273e1
Michael <233148174+psyhoopat@users.noreply.github.com>
2025-11-10 20:09:22 +0300

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,15 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
 
-from mysite.forms import SingUpForm
-
+from mysite.forms import SingUpForm, LoginForm
 
 # Create your views here.
 def index(request):
     context = {
         "username": "admin",
-        "is_auth": False,
+        "is_auth": request.user.is_authenticated,
         "links_not_auth": {
             "register": "Регистрация",
             "login": "Войти",
@@ -45,11 +46,33 @@ def register(request):
 
     return render(request, "page/register.html", {"form": form})
 
-def login(request):
-    return render(request, "page/login.html")
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect("/profile")
+            else:
+                return render(request, "page/login.html", {"form": form})
+    else:
+        form = LoginForm()
 
+    return render(request, "page/login.html", {"form": form})
+
+@login_required
 def profile(request):
     return render(request, "page/profile.html")
 
 def load_image(request):
     return HttpResponse("Page load image.")
+
+def logout_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/login")
+
+    logout(request)
+    return HttpResponseRedirect("/login")
