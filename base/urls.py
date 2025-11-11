d1b70a203ab56f5f0675f16076919ef1e128293f
psyhoopat <233148174+psyhoopat@users.noreply.github.com>
2025-11-11 09:37:07 +0300

    update base_dir static; add pillow requirements.txt;

--- base/urls.py
+++ base/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('mysite.urls')),
     path('image/', include('image.urls')),
     path('admin/', admin.site.urls),
 ]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
