460294af10a92cf776201ccab75ff25faaaccfcf
Michael <233148174+psyhoopat@users.noreply.github.com>
2025-11-20 17:07:22 +0300

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.TextField()
-    cover = models.ImageField(upload_to='images/')
+    title = models.TextField() # имя файла
+    cover = models.ImageField(upload_to='images/') # путь к файлу
     user_id = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-    )
+    ) # кто пользователь
 
     def __str__(self):
-        return self.title
+        return self.title # каждая запись будет именовано пользовательским вводом
