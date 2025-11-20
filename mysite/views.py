83b1f6a7109716b6573b54c59960912e4520d1a4
Michael <233148174+psyhoopat@users.noreply.github.com>
2025-11-20 17:04:02 +0300

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -11,6 +11,7 @@ from .models import Post
 
 # Create your views here.
 def index(request):
+    # передать статус аутентифицированного юзера
     context = {
         "username": "admin",
         "is_auth": request.user.is_authenticated,
@@ -19,74 +20,90 @@ def index(request):
     return render(request, "page/main.html", context)
 
 def register(request):
-    # Как создать регистрацию: https://fixmypc.ru/post/sozdaem-stranitsu-registratsii-polzovatelei-django/?ysclid=mhsztne413664128872
+    # как создать регистрацию: https://fixmypc.ru/post/sozdaem-stranitsu-registratsii-polzovatelei-django/?ysclid=mhsztne413664128872
+
+    # если пользователь аутентифицирован перенаправим на страницу профиля
     if request.user.is_authenticated:
         return HttpResponseRedirect("/profile")
 
     if request.method == "POST":
-        form = SingUpForm(request.POST)
+        form = SingUpForm(request.POST) # передать данные в форму
         if form.is_valid():
+            # получаем данные
             username = form.cleaned_data['username']
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password1"]
 
+            # получить юзера из модели
             user = User.objects.create_user(username=username, email=email, password=password)
 
+            # добавим значения и сохраним в базу данных
             user.first_name = first_name
             user.last_name = last_name
             user.save()
 
+            # редирект на логин
             return HttpResponseRedirect("/login")
     else:
-        form = SingUpForm()
+        form = SingUpForm() # создать обычную форму
 
     context = { "form": form }
     return render(request, "page/register.html", context)
 
 def login_view(request):
+
+    # если пользователь аутентифицирован перенаправим на страницу профиля
     if request.user.is_authenticated:
         return HttpResponseRedirect("/profile")
 
     if request.method == "POST":
         form = LoginForm(data=request.POST or None)
         if form.is_valid():
+            # получить данные
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
+
+            # аутентификация
             user = authenticate(username=username, password=password)
+
+            # проверка на аутентификацию
             if user is not None:
-                login(request, user)
+                login(request, user) # войти
                 return HttpResponseRedirect("/profile")
             else:
+                # пользователь не определен
                 return render(request, "page/login.html", {
                     "form": form,
                     "error": True,
                     "message": "Неправильный пользователь и/или пароль."
                 })
     else:
-        form = LoginForm()
+        form = LoginForm() # создать форму
 
     context = { "form": form }
     return render(request, "page/login.html", context)
 
 @login_required
 def profile(request):
-    object_list = Post.objects.filter(user_id=request.user.id)
+    object_list = Post.objects.filter(user_id=request.user.id) # только пользовательские картинки
 
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
+            # получить данные
             title = form.cleaned_data["title"]
             cover = form.cleaned_data["cover"]
 
+            # получить пользователя и создать новую картинку
             user = User.objects.get(pk=request.user.pk)
             obj = Post.objects.create(title=title, cover=cover, user_id=user)
             obj.save()
 
             return HttpResponseRedirect("/profile")
     else:
-        form = PostForm()
+        form = PostForm() # создать форму
 
     context = {
         "is_auth": request.user.is_authenticated,
