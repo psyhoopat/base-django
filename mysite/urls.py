451b8e85522cc01c608aaa4de190c3f101048f06
Michael <233148174+psyhoopat@users.noreply.github.com>
2025-11-20 17:05:32 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('registration/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # чтобы картинки подгружались
